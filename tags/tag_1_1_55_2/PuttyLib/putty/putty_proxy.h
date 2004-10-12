482375ae07959b4041adb9042e94251e214d2e44
kanci <kanci@86055b97-dde4-0310-95f8-9d1975025dd6>
2004-10-12 19:14:01 +0000

    made a copy

---
+++ tags/tag_1_1_55_2/PuttyLib/putty/putty_proxy.h
@@ -0,0 +1,12 @@
+#ifndef _PUTTY_PROXY_H
+#define _PUTTY_PROXY_H
+
+enum {
+    /*
+     * Proxy types.
+     */
+    PROXY_NONE, PROXY_SOCKS4, PROXY_SOCKS5,
+    PROXY_HTTP, PROXY_TELNET, PROXY_CMD
+};
+
+#endif //_PUTTY_PROXY_H
